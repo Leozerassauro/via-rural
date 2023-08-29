f5c24b27201728b557f3b2ba5428887682e8bc9e
Leonardo <leo.girardi97@hotmail.com>
2023-08-29 13:48:25 -0300

    added svg-transformer

---
+++ mobile/src/theme/light.ts
@@ -0,0 +1,16 @@
+// Native
+import { DefaultTheme } from 'react-native-paper'
+
+export const lightTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#25621E',
+    secondary: '#3B9D30',
+    tertiary: '#163A12',
+
+    error: '#EB0000',
+
+    background: '#FFFFFF',
+  },
+}
