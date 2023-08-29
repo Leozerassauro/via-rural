ada49c1193fd8ee76cac9bdf110c7657cdc3e6d8
Leonardo <leo.girardi97@hotmail.com>
2023-08-29 13:33:22 -0300

    added babel-plugin-module-resolver

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@configs': './src/configs',
+            '@contexts': './src/contexts',
+            '@dtos': './src/dtos',
+            '@hooks': './src/hooks',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@storage': './src/storage',
+            '@routes': './src/routes',
+            '@themes': './src/themes',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  }
+}
